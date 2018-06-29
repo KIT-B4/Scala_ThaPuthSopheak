9b9c8f0ee41d7965ea88e185b4d23244ccbf4fb7
puthsopheak <34528278+puthsopheak@users.noreply.github.com>
2018-06-29 11:31:28 +0700

    loop year

---
+++ loop_year.scala
@@ -0,0 +1,13 @@
+object MainObject {  
+   def main(args: Array[String]) {  
+     val a=scala.io.StdIn.readInt()
+     var i: Int =10;
+     while (i!=0)
+     {
+     	println(i)
+     	i-=1
+     }
+	println("Happy birthday") 
+	println(a);
+   }  
+}  
